938744f2cc9d1eb83190f3f121555722f051aecb
anicolao <anicolao@users.noreply.github.com>
2026-08-14 15:34:34 +0000

    deploy: 74a871a4e956b019dee85315bd50eb61b93e0048

---
+++ pr6/_app/immutable/nodes/2.BcLrqyo_.js
@@ -0,0 +1,1 @@
+import{t as e}from"../chunks/BxBxvhCW.js";export{e as component};
---
+++ pr6/_app/immutable/nodes/4.MgtfKc4q.js
@@ -0,0 +1,1 @@
+import{dt as e,it as t}from"../chunks/B_X-KhPJ.js";import"../chunks/xihTtKlq.js";import{t as n}from"../chunks/BxBxvhCW.js";var r=e({trailingSlash:()=>i}),i=`always`;t();function a(e){n(e,{tabletopRoute:!0})}export{a as component,r as universal};
